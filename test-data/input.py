113e974dad1ba3d8b173701b5f3dd4a2c029cc89
Amarpreet Singh <41732341+code-100@users.noreply.github.com>
2019-10-30 21:50:18 +0530

    Add files via upload

---
+++ test-data/input.py
@@ -0,0 +1,55 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from matplotlib.pyplot import specgram
+import keras
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Input, Flatten, Dropout, Activation
+from keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import confusion_matrix
+from keras.models import model_from_json
+import os
+import pandas as pd
+import librosa
+import glob
+
+json_file = open('model_json.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+loaded_model.load_weights("Emotion_Model.h5")
+ 
+opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)
+loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+path = input(str("enter path: "))
+
+X, sample_rate = librosa.load('{}'.format(path), res_type='kaiser_fast', duration=2.5, sr=22050*2, offset=0.5)
+sample_rate = np.array(sample_rate)
+mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13),axis=0)
+featurelive = mfccs
+livedf2 = featurelive
+
+twodim= np.expand_dims(livedf2, axis=2)
+
+livepreds = loaded_model.predict(twodim, batch_size=32, verbose=1)
+livepreds1=livepreds.argmax(axis=1)
+liveabc = livepreds1.astype(int).flatten()
+livepredictions = (lb.inverse_transform((liveabc)))
+print(livepredictions)
+
+preddf = pd.DataFrame({'predictedvalues': predictions})
+preddf.to_csv('Predictions.csv', index=False)
+
+
